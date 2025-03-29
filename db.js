f961e8f83afa89c5cc82c2fedc555569bf5d732e
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2025-03-28 22:00:48 -0300

    Aula 20-03-2025

---
+++ db.js
@@ -0,0 +1,25 @@
+// Importa para interagir com o banco de dados
+const mongoose = require("mongoose");
+
+// Carrega váriaveis de ambiente do arquivo .ENV
+require("dotenv").config();
+
+// Configura o mongoose para permitir  consulta (Restritas)
+mongoose.set("strictQuery", true);
+
+// Função para conectar ao DB
+async function main() {
+  await mongoose
+    .connect
+    // Link do DB
+    ();
+
+  // Exibe a mensagem ao úsuario que realizou a conexão
+  console.log("Conectou ao banco de dados!");
+}
+
+// Caso ocorra erro mostra uma mensagem
+main().catch((err) => console.log(err));
+
+// Exportar a função para utilizar em outro arquivo
+module.exports = main;
